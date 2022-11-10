04ae9991466cf8b3791c70376e449a02dc25a350
jeonhyoungmin <jhmin7509@gamil.com>
2022-11-11 03:12:38 +0900

    가상환경 설정

---
+++ python-di.py
@@ -0,0 +1,13 @@
+print('hi')
+
+# 자바스크립트는 package.json "파일"로 관리되지만,
+# 파이썬은 시스템을 다루는 것이 기본(웹은 관련 없음)이라 시스템을 정의하는 일련의 파일이 생성된다.
+#  가상환경을 세팅하면 python -m vevn exampleOne ->
+# exampleOne 이라는 폴더가 생성되고
+# bin
+# include
+# lib
+# 세 개의 폴더가 생성된다. 이것은 node_modules와 비슷한 맥락이다.
+#  바깥에서 만들지 않는다.
+
+# 본 영상에서는 exampleOne 이라는 "생성된 폴더가" root 디렉토리가 된다.
